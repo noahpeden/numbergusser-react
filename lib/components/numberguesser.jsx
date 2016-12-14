8c9c38129143f46d443abc555205ad09c02e5b06
Noah Peden <noahpeden@gmail.com>
2016-12-14 15:55:38 -0700

    add message field

--- lib/components/numberguesser.jsx
+++ lib/components/numberguesser.jsx
@@ -4,21 +4,35 @@ export default class NumberGuesser extends React.Component {
   constructor(){
     super();
     this.state = {
-      lastGuess: null,
-      answer: 42
+      lastGuess: '',
+      answer: 42,
+      guess: ''
     };
   }
+  handleSubmit(){
+    console.log(this.state.guess);
+    this.setState({
+      lastGuess: this.state.guess
+    })
+  }
+  updateGuess(e){
+    console.log(e.target.value);
+    this.setState({
+      guess: e.target.value
+    })
+  }
+
   render(){
     return (
       <section className="NumberGuesser">
         <div className="LastGuess">
           <p>Your last guess was...</p>
-          <p>13</p>
+          <p>{this.state.lastGuess}</p>
           <p>That number was too low. Try again.</p>
         </div>
         <div className="Controls">
-          <input type="number" placeholder="Your best guess" />
-          <button>Guess</button>
+          <input onChange={(e)=> this.updateGuess(e)} type="number" placeholder="Your best guess" />
+          <button onClick={(e)=> this.handleSubmit()}>Guess</button>
           <button>Clear</button>
           <button>Reset Game</button>
         </div>
