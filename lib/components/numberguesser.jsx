b1cbf8107c28e05e33bfff20e8673990293fb719
Noah Peden <noahpeden@gmail.com>
2017-01-03 08:18:57 -0700

    all phase one requirements minus css

--- lib/components/numberguesser.jsx
+++ lib/components/numberguesser.jsx
@@ -5,36 +5,79 @@ export default class NumberGuesser extends React.Component {
     super();
     this.state = {
       lastGuess: '',
-      answer: 42,
+      answer: this.randomNumber(),
       guess: ''
     };
   }
+
+  randomNumber(){
+  let min = 1;
+  let max = 100;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
   handleSubmit(){
-    console.log(this.state.guess);
+    console.log(this.state.answer);
     this.setState({
       lastGuess: this.state.guess
     })
   }
   updateGuess(e){
-    console.log(e.target.value);
     this.setState({
       guess: e.target.value
     })
   }
 
+  clearBtn(e){
+    this.refs.inputGuess.value = '';
+    }
+
+  returnGuess(){
+    if(this.state.lastGuess === ''){
+      return(
+        <p></p>
+      )
+    }
+    if(this.state.lastGuess == this.state.answer){
+      return(
+        <p>Correct, you win!</p>
+      )
+    }
+    if(this.state.lastGuess < this.state.answer){
+      return(
+      <p>"Sorry, that guess is too low. Try a higher number."</p>
+    )
+  }
+   if(this.state.lastGuess > this.state.answer){
+    return (
+      <p>"Sorry, that guess is too high. Try a lower number."</p>
+    )
+    }
+  }
+
+  resetBtn(){
+    this.setState({
+      answer: this.randomNumber(),
+      lastGuess: ''
+    })
+    this.clearBtn()
+  }
+
+
+
   render(){
     return (
       <section className="NumberGuesser">
         <div className="LastGuess">
-          <p>Your last guess was...</p>
-          <p>{this.state.lastGuess}</p>
-          <p>That number was too low. Try again.</p>
+          <article>Your last guess was...</article>
+          <article>{this.state.lastGuess}</article>
+          <article>{this.returnGuess()}</article>
         </div>
         <div className="Controls">
-          <input onChange={(e)=> this.updateGuess(e)} type="number" placeholder="Your best guess" />
-          <button onClick={(e)=> this.handleSubmit()}>Guess</button>
-          <button>Clear</button>
-          <button>Reset Game</button>
+          <input ref='inputGuess' onChange={(e) => this.updateGuess(e)} type="number" placeholder="Your best guess" />
+          <button onClick={(e) => this.handleSubmit()}>Guess</button>
+          <button onClick={(e) => this.clearBtn(e)}>Clear</button>
+          <button onClick={(e) => this.resetBtn(e)}>Reset Game</button>
         </div>
       </section>
     )
